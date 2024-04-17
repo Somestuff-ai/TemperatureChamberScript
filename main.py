13dd7114725d1e0c4cd6359bc913ea87328dce3e
Somestuff-ai <rahuldabawala@hotmail.com>
2024-04-17 15:13:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import serial
 
 # Opens serial port
-ser = serial.Serial ('COM3',9600, 8, 'n', 1) 
-ser.open()
+ser = serial.Serial ('COM3',9600) 
 
-command = bytearray('\x0401v000a10\x03$', 'ascii')
+
+command = bytearray('\x0401v000a10\x03$', 'ascii') #uses XOR checksum 
 ser.write(command)
 
-ser.close()
+
 
 
