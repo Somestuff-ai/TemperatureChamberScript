f1d88b14fb1de794a9b3f051ba8e93f69f9bb2bb
Somestuff-ai <67798785+Somestuff-ai@users.noreply.github.com>
2024-04-17 14:52:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import serial
 ser = serial.Serial ('COM3',9600, 8, 'n', 1) 
 ser.open()
 
-command = bytearray('\x04V000a10\x03', 'ascii')
+command = bytearray('\x0401v000a10\x03$', 'ascii')
 ser.write(command)
 
 ser.close()
