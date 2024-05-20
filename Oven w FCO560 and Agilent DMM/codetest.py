86a6eec8748df55a474b3f84cfad6be47dde7a24
Somestuff-ai <67798785+Somestuff-ai@users.noreply.github.com>
2024-05-20 13:58:16 +0100

    Update codetest.py

--- Oven w FCO560 and Agilent DMM/codetest.py
+++ Oven w FCO560 and Agilent DMM/codetest.py
@@ -13,22 +13,96 @@ from initialise import csv_file_path, serial_connections, device
 # print(hex_bytes.hex())
 
 
+step = 3
+def venus_send_command():
 
-def tt10_send_enquiry():
-    ser = serial_connections[2]
-    command = b'\x01\x10\x80\x04\x00\x02\x04\x41\x20\x00\x00\x86\x6C'
-    if not ser.is_open:
-        ser.open()
+    if step == 1:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x00\x00\x00\x00\x93\x9A'# Command for 0°C set point
+        if not ser.is_open:
+            ser.open()
 
-    ser.write(command)
+        ser.write(command)
 
-    time.sleep(1)
-    response = ser.read_all()
 
-    substr = response.decode('utf-8')
-    response = substr[2:8]
+        ser.close()        
+
+    elif step == 2:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x41\x20\x00\x00\x86\x6C' # Command for 10°C set point
+        if not ser.is_open:
+            ser.open()
+
+        ser.write(command)
+
+
+        ser.close()
+
+    elif step == 3:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x41\xA0\x00\x00\x87\x84' #Command for 20°C set point
+        if not ser.is_open:
+            ser.open()
+
+        ser.write(command)
+
+
+        ser.close()
+
+    elif step == 4:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x42\x0C\x00\x00\x47\xE1' #Command for 35°C set point
+        if not ser.is_open:
+            ser.open()
+
+        ser.write(command)
+
+
+        ser.close()    
+
+    elif step == 5:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x42\x32\x00\x00\x46\xC4' #Command for 50°C set point
+        if not ser.is_open:
+            ser.open()
+
+        ser.write(command)
+
+
+        ser.close()  
+
+    elif step == 6:
+        ser = serial_connections[2]
+        command = b'\x01\x10\x80\x04\x00\x02\x04\x41\xA0\x00\x00\x87\x84' #Command for 50°C set point
+        if not ser.is_open:
+            ser.open()
+
+        ser.write(command)
+
+
+        ser.close()        
+
+
+venus_send_command()
+
+
+
+def agilent_send_enquiry():
+
+    ser = serial_connections[device]
+
+    commands = [
+    b'SYST:REMOTE\r\n',
+    b'*CLS\r\n',
+    b'func "FRES"\r\n',
+    b'conf:FRES DEF,DEF\r\n',
+    b'READ?\r\n'
+] 
+    for command in commands:
+        ser.write(command)  
+
+    time.sleep(1)  
+
+    response = ser.read(1000)
 
     ser.close()
-
-    return response
-tt10_send_enquiry()
