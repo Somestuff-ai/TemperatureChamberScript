ce925f9dfd81f3e851478808dea5af06477b317f
Somestuff-ai <67798785+Somestuff-ai@users.noreply.github.com>
2024-05-20 09:36:11 +0100

    Update codetest.py

--- Oven w FCO560 and Agilent DMM/codetest.py
+++ Oven w FCO560 and Agilent DMM/codetest.py
@@ -1,3 +1,33 @@
-char = "\ü"
-hex_representation = char.encode("utf-8").hex()
-print(hex_representation)
+import serial
+from datetime import datetime, timedelta
+import time
+from initialise import csv_file_path, serial_connections, device
+
+# char = "\ü"
+# hex_representation = char.encode("utf-8").hex()
+# print(hex_representation)
+
+serial_command = "^A^P€^D^@^B^D^@^@^@^@“š"
+hex_bytes = bytes(serial_command.encode('utf-8'))
+
+print(hex_bytes.hex())
+
+
+
+def tt10_send_enquiry():
+    ser = serial_connections[device]
+    enq = b'\x5c\xfc'
+    if not ser.is_open:
+        ser.open()
+
+    ser.write(enq)
+
+    time.sleep(1)
+    response = ser.read_all()
+
+    substr = response.decode('utf-8')
+    response = substr[2:8]
+
+    ser.close()
+
+    return response
